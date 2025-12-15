950eb02efa298772293e60c080323a7a85e6400e
Raza Shaikh <123723336+razashaikh99@users.noreply.github.com>
2025-12-15 21:13:58 +0500

    update formik

---
+++ src/Sections/ContactUs/ContactSchema/ValidationSchema.js
@@ -0,0 +1,19 @@
+import * as yup from 'yup';
+
+export const contactSchema = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    tel: '',
+    subject: '',
+    message: ''
+};
+
+export const contactValidationSchema = yup.object().shape({
+    firstName: yup.string().required('First Name Required...'),
+    lastName: yup.string().required('Last Name Required...'),
+    email: yup.string().email().required('Email Required...'),
+    tel: yup.string().required('Contact No Required...').matches(/^[0-9+ ]+$/, 'Invalid Phone Number'),
+    subject: yup.string().required('Subject Required...'),
+    message: yup.string().required('Message Required...')
+})
